386c85b63a9368a73b27705f3398e2b334ddc4aa
fanny.maras <fanny.petterssonsallberg@astrazeneca.com>
2023-06-27 11:09:42 +0200

    on its way

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-type Project = {
-  title: string;
-  description: string;
-  contact: string;
-  tags: string[];
-};
+import { Project } from "../types/index";
+import { useCollection, useFirestore } from "vuefire";
+import { collection } from "firebase/firestore";
+
+const projectsCollection = useCollection(collection(useFirestore(), "project"));
+
+console.log("collection", projectsCollection);
 
 const projects = ref<Project[]>([]);
 
@@ -22,17 +23,113 @@ const showProject = (project: Project) => {
   selectedproject.value = project;
 };
 
-try {
-  console.log("IN INDEX.VUE GETTING DATA");
-  const { data, error } = await useFetch("/api/projects");
-  projects.value = (data.value as Project[]) ?? [];
-  if (error.value) {
-    throw new Error(error.value.message);
-  }
-  console.log(data);
-} catch (error) {
-  console.error(error);
-}
+console.log("IN INDEX.VUE GETTING DATA");
+console.log("projects collection, ", projectsCollection);
+console.log("projects, ", projects);
+
+/*
+
+export default {
+  data() {
+    return {
+      title: "",
+      description: "",
+      tags: [],
+      search: "",
+      projectsData: [],
+    };
+  },
+  firestore: {
+    projectsData: db.collection("projects"),
+  },
+  methods: {
+    createProject(
+      title: string,
+      description: string,
+      contact: string,
+      tags: Array<string>
+    ) {
+      if (title != "" || description != "" || contact != "") {
+        db.collection("employees")
+          .add({
+            title: title,
+            description: description,
+            contact: contact,
+            tags: tags,
+          })
+          .then(() => {
+            console.log("Document successfully written!");
+            this.readProjects();
+          })
+          .catch((error: Error) => {
+            console.error("Error writing document: ", error);
+          });
+        // this.title = "";
+        // this.description = "";
+        // this.contact = "";
+        // this.tags = "";
+      }
+    },
+    readProjects() {
+      let projectsData: Project[] = [];
+      db.collection("projects")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc: Project) => {
+            projectsData.push({
+              title: doc.title,
+              description: doc.description,
+              contact: doc.contact,
+              tags: doc.tags,
+            });
+            console.log(doc.title, " => ", doc);
+          });
+          return projectsData;
+        })
+        .catch((error: Error) => {
+          console.log("Error getting documents: ", error);
+        });
+    },
+    updateProject(
+      title: string,
+      description: string,
+      contact: string,
+      tags: Array<string>
+    ) {
+      db.collection("projects")
+        .doc(title)
+        .update({
+          title: title,
+          description: description,
+          contact: contact,
+          tags: tags,
+        })
+        .then(() => {
+          console.log("Document successfully updated!");
+          this.readProjects();
+        })
+        .catch((error: Error) => {
+          // The document probably doesn't exist.
+          console.error("Error updating document: ", error);
+        });
+    },
+    deleteProject(title: string) {
+      db.collection("projects")
+        .doc(title)
+        .delete()
+        .then(() => {
+          console.log("Document successfully deleted!");
+          this.readProjects();
+        })
+        .catch((error: Error) => {
+          console.error("Error removing document: ", error);
+        });
+    },
+  },
+  mounted() {
+    this.readProjects();
+  },
+}; */
 </script>
 
 <template>
@@ -42,7 +139,7 @@ try {
     </div>
 
     <div class="grid grid-cols-4 gap-4">
-      <div v-for="project in projects">
+      <div v-for="project in projects" :key="project.id">
         <ProjectCard :project="project" @setSelectedProject="showProject" />
       </div>
     </div>
