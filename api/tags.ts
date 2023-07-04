554ee57c839cfd0f546fd92c810a99d4eeb07aea
Magnus Karlsson <magnus.karlsson@cygni.se>
2023-07-04 14:52:25 +0200

    added api routes for firestore

--- api/tags.ts
+++ api/tags.ts
@@ -1,11 +1,9 @@
 import { collection } from "firebase/firestore";
 import { useCollection, useFirestore } from "vuefire";
 
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async (event: any) => {
   try {
-    const data = useCollection(
-      collection(useFirestore(), "competence-projects")
-    );
+    const data = useCollection(collection(useFirestore(), "competence-tags"));
     return data;
   } catch (error) {
     console.log(error);
