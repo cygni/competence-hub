a70a00f4fddac20ea7f08d83596f24a2835407fe
Magnus Karlsson <magnus.karlsson@cygni.se>
2023-07-04 15:05:58 +0200

    updated tags component

--- components/Tag.vue
+++ components/Tag.vue
@@ -1,16 +1,25 @@
 <template>
-  <span
-    :id="'tag-' + tag"
-    class="text-white p-2 mr-2 mb-2 rounded-xl"
+  <div
+    class="text-white p-2 mr-2 mb-2 rounded-xl inline-flex transition max-w-20 group"
     :style="{ 'background-color': randomBackground(tag) }"
   >
-    {{ tag.value ? tag.value : tag }}
-  </span>
+    <div :id="'tag-' + tag">
+      {{ tag.value ? tag.value : tag }}
+    </div>
+    <div
+      v-if="edit"
+      class="cursor-pointer transition duration-350 max-w-0 opacity-0 group-hover:max-w-full group-hover:opacity-100"
+      @click="$emit('removeTag', tag)"
+    >
+      <img src="assets/images/close-white.svg" alt="delete" width="25" />
+    </div>
+  </div>
 </template>
+
 <script setup lang="ts">
 import { TechTag } from "../types";
+defineProps<{ tag: TechTag; edit?: boolean }>();
 
-defineProps<{ tag: TechTag }>();
 /* Cygni brand colors */
 const fullstackColors = ["#000735", "#440B45"];
 const backendColors = ["#00b3b0", "#00966d"];
@@ -26,25 +35,25 @@ const colors = [
   "#00966d",
   "#f9e79f",
 ];
-const randomBackground = (tag) => {
-  if (tag.aspect) {
-    switch (tag.aspect) {
-      case "backend": {
-        const randomIndex = Math.floor(Math.random() * backendColors.length);
-        return backendColors[randomIndex];
-      }
-      case "frontend": {
-        const randomIndex = Math.floor(Math.random() * frontendColors.length);
-        return frontendColors[randomIndex];
-      }
-      case "fullstack": {
-        const randomIndex = Math.floor(Math.random() * fullstackColors.length);
-        return fullstackColors[randomIndex];
-      }
-    }
-  } else {
+const randomBackground = (tag: TechTag) => {
+  if (!tag.aspect) {
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
+  }
+
+  switch (tag.aspect) {
+    case "backend": {
+      const randomIndex = Math.floor(Math.random() * backendColors.length);
+      return backendColors[randomIndex];
+    }
+    case "frontend": {
+      const randomIndex = Math.floor(Math.random() * frontendColors.length);
+      return frontendColors[randomIndex];
+    }
+    case "fullstack": {
+      const randomIndex = Math.floor(Math.random() * fullstackColors.length);
+      return fullstackColors[randomIndex];
+    }
   }
 };
 </script>
