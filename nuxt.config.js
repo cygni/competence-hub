d4dbe07769185f3caba8dbe03f20723e150ab08a
fanny.maras <fanny.petterssonsallberg@astrazeneca.com>
2023-07-06 13:27:43 +0200

    test if vercel works now

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,19 +26,21 @@ export default defineNuxtConfig({
     },
     admin: {
       serviceAccount: {
-        type: "service_account",
-        project_id: process.env.FIREBASE_PROJECT_ID,
-        private_key_id: process.env.FIREBASE_KEY_ID,
-        private_key: process.env.FIREBASE_PRIVATE_KEY
-          ? process.env.FIREBASE_PRIVATE_KEY.replace(/\n/gm, "\n")
-          : undefined,
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
-        client_id: process.env.FIREBASE_CLIENT_ID,
-        auth_uri: process.env.FIREBASE_AUTH_URI,
-        token_uri: process.env.FIREBASE_TOKEN_URI,
-        auth_provider_x509_cert_url:
-          process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URI,
-        client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URI,
+        credential: {
+          type: "service_account",
+          project_id: process.env.FIREBASE_PROJECT_ID,
+          private_key_id: process.env.FIREBASE_KEY_ID,
+          private_key: process.env.FIREBASE_PRIVATE_KEY
+            ? process.env.FIREBASE_PRIVATE_KEY.replace(/\n/gm, "\n")
+            : "",
+          client_email: process.env.FIREBASE_CLIENT_EMAIL,
+          client_id: process.env.FIREBASE_CLIENT_ID,
+          auth_uri: process.env.FIREBASE_AUTH_URI,
+          token_uri: process.env.FIREBASE_TOKEN_URI,
+          auth_provider_x509_cert_url:
+            process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URI,
+          client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URI,
+        },
       },
     },
   },
