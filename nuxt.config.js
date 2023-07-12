015b91699c25590b2099f9d759b8a9a2c89a889a
fanny.maras <fanny.petterssonsallberg@astrazeneca.com>
2023-07-12 11:02:05 +0200

    removed config file not needed

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import ServiceAccount from "./config/serviceAccount.js";
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -28,7 +27,20 @@ export default defineNuxtConfig({
       measurementId: "G-JY96C7V423",
     },
     admin: {
-      serviceAccount: ServiceAccount,
+      serviceAccount: JSON.stringify({
+        type: "service_account",
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        private_key_id: process.env.FIREBASE_KEY_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY,
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        client_id: process.env.FIREBASE_CLIENT_ID,
+        auth_uri: process.env.FIREBASE_AUTH_URI,
+        token_uri: process.env.FIREBASE_TOKEN_URI,
+        auth_provider_x509_cert_url:
+          process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URI,
+        client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URI,
+        universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+      }),
     },
   },
 });
